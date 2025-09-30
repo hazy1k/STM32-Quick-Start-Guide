6664cbe6f9d81e26d5bf645613f072ab65afa1fb
hazy1k <a249007421@gmail.com>
2025-09-30 23:38:26 +0800

    正点原子H750emWin例程

---
+++ Totoal-Reference-Material/参考代码/正点原子H750/emWin例程/emWin所需要的文件/SeggerEval_WIN32_MSVC_MinGW_GUI_V622/APP_Image/music.h
@@ -0,0 +1,35 @@
+ /**
+ ****************************************************************************************************
+ * @file        music.h
+ * @author      正点原子团队(ALIENTEK)
+ * @version     V1.0
+ * @date        2020-8-24
+ * @brief       music 驱动代码
+ * @license     Copyright (c) 2020-2032, 广州市星翼电子科技有限公司
+ ****************************************************************************************************
+ * @attention
+ *
+ * 实验平台:正点原子 STM32F103开发板
+ * 在线视频:www.yuanzige.com
+ * 技术论坛:www.openedv.com
+ * 公司网址:www.alientek.com
+ * 购买地址:openedv.taobao.com
+ *
+ * 修改说明
+ * V1.0 20200417
+ * 第一次发布
+ *
+ ****************************************************************************************************
+ */
+
+#ifndef _MUSIC_H
+#define _MUSIC_H
+#include "GUI.h"
+
+#ifndef GUI_CONST_STORAGE
+  #define GUI_CONST_STORAGE const
+#endif
+
+extern GUI_CONST_STORAGE GUI_BITMAP bmmusic;
+extern GUI_CONST_STORAGE GUI_BITMAP bmwifi;
+#endif
